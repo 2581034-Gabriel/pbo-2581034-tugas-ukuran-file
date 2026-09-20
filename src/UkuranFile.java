d4b3b1f833feeef01a375a35e331bb74654b6fb6
Vivaldhy Gabriel Valentino P.Silitonga <2581034@unai.edu>
2026-09-20 23:16:07 +0700

    Menambahkan kalkulator ukuran file

--- src/UkuranFile.java
+++ src/UkuranFile.java
@@ -5,7 +5,7 @@ public class UkuranFile {
         Scanner input = new Scanner(System.in);
 
         String namaFile;
-        // variabel untuk menyimpan teks
+       // variabel untuk menyimpan teks
 
         System.out.print("Nama file     : ");
         namaFile = input.nextLine();
@@ -16,6 +16,23 @@ public class UkuranFile {
             System.out.print("Nama file     : ");
             namaFile = input.nextLine();
         }
+
+        // Ukuran file menggunakan long karena nilainya bisa besar, jika menggunakan int ukuran file bisa melebihi batas int
+        System.out.print("Ukuran (byte) : ");
+        long ukuranByte = input.nextLong();
+
+        final double SATU_KB = 1024.0;
+        // final artinya nilai tidak dapat diubah
+        // 1024.0 digunakan agar java melakukan pembagian menggunakan tipe data double sehingga melakukan pembagian angka dengan desimal
+
+        double ukuranKB = ukuranByte / SATU_KB;
+        double ukuranMB = ukuranKB / SATU_KB;
+        double ukuranGB = ukuranMB / SATU_KB;
+
+        // (int) mengubah double menjadi int dan menghilangkan angka di belakang koma
+        int ukuranMBBulat = (int) ukuranMB;
+
+        double selisih = ukuranMB - ukuranMBBulat;
 
     }
 }
